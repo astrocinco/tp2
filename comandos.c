b3fedf2dd99098208363aae9ece3f1a9c089479d
astrocinco <santiagolps3@gmail.com>
2021-12-01 20:45:59 -0300

    .

--- comandos.c
+++ comandos.c
@@ -3,6 +3,26 @@
 void login(){
     return;
 }
+// PROBABLEMENTE NO VAYA VOID. PONER TIPO CORRECTO EN .H Y .C
+void logout(){
+
+}
+
+void publicar(){
+
+}
+// PROBABLEMENTE NO VAYA VOID. PONER TIPO CORRECTO EN .H Y .C
+void ver_prox(){
+
+}
+
+void likear(){
+
+}
+// PROBABLEMENTE NO VAYA VOID. PONER TIPO CORRECTO EN .H Y .C
+void ver_likes(){
+
+}
 
 void debugger(){
     printf("Debugger\n");
--- comandos.h
+++ comandos.h
@@ -1,7 +1,17 @@
 #ifndef _COMANDOS_H_
 #define _COMANDOS_H_
 
-void login();
+void login(); 
+// PROBABLEMENTE NO VAYA VOID. PONER TIPO CORRECTO EN .H Y .C
+void logout();
+
+void publicar();
+
+void ver_prox();
+
+void likear();
+// PROBABLEMENTE NO VAYA VOID. PONER TIPO CORRECTO EN .H Y .C
+void ver_likes();
 
 void debugger();
 
