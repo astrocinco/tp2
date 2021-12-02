aeb98b476eccb903dc903c557b94b4f874873a84
Joaquin <jorivero@fi.uba.ar>
2021-12-02 03:01:19 -0300

    Add files via upload

--- comandos.c
+++ comandos.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 
-void login(){
-    return;
+
+
+
+
+
+
+
+void login(hash_t* usuarios, usuario_t* usuario_activo){
+    if (usuario_activo != NULL){
+        printf("Error: Ya habia un usuario loggeado\n" );
+    }
+    char* user;
+    int buffer;
+    int longitud = getline(&user,&buffer,stdin);
+
+    if (hash_pertenece(usuarios,stdin)){
+        usuario_activo = hash_obtener(usuarios,stdin);
+        printf("Hola %s\n",line);
+    }else{
+        printf("Error: usuario no existente\n");
+    }
 }
 // PROBABLEMENTE NO VAYA VOID. PONER TIPO CORRECTO EN .H Y .C
-void logout(){
-
+void logout(usuario_t* usuario_activo){
+    if (usuario_activo != NULL){
+        usuario_activo = NULL;
+        printf("Adios\n");
+    }else{
+        printf("Error: no habia usuario loggeado.\n");
+    }
 }
 
 void publicar(){
--- main.c
+++ main.c
@@ -23,7 +23,9 @@
         - ¿GETLINE LE PASAMOS UN PUNTERO CADA VEZ? O USAMOS SIEMPRE EL MISMO PUNTERO Y COPIAMOS SU CONTENIDO EN EL LUGAR CORRECTO
         - PARA GUARDAR USUARIO LOGEADO: GUARDAR COMO VARIABLE EN esperar_orden ¿no? Y CUANDO NO HAYA NADIE LOGEADO usuario_actual = NULL
         - CORTAR LOOP CON ORDEN EN esperar_orden (LLAMANDO esperar_orden SOLO UNA VEZ DESDE MAIN) O CORTAR CON UN BOOL EN main que se MODIFIQUE DESDE esperar_orden CON PUNTERO
-        - INIESTA TIENE MISMA AFINIDAD CON PIQUE QUE CON GUARDIOLA?           SOLUCIONADO -- > NO DA LA VUELTA J
+        LISTO - INIESTA TIENE MISMA AFINIDAD CON PIQUE QUE CON GUARDIOLA?           SOLUCIONADO -- > NO DA LA VUELTA J
+
+        - se guarda el logeado en el main o hacemos un struct
 
 --------------------------------------------------------------------*/
 
@@ -40,7 +42,7 @@
 #define NRO_ARGUMENTOS_INGRESO_TXT 2
 #define ARGUMENTO_NOMBRE_ARCHIVO 1
 #define TAM_MAX_INGRESO 150
-//#define _POSIX_C_SOURCE 200809L
+#define _POSIX_C_SOURCE 200809L
 
 // --- STRUCTS
 
@@ -49,7 +51,7 @@ typedef struct post{
     size_t nro_id;
     char* creador;
     char* contenido;
-    abb_t* likes;
+    lista_t* likes; //lo hice lista asi mientras lo recorremos mostrando y es O(u)
 } post_t;
 
 
@@ -69,19 +71,27 @@ typedef struct usuario{
 //  --- FUNCIONES
 
 
-void esperar_orden(){
+void esperar_orden(hash_t* usuarios){
     bool terminar = false;
 	char* ingreso = malloc(sizeof(char) * TAM_MAX_INGRESO);
 	int tam_buffer = 1000; // HACER CONSTANTE O VER DE DONDE SACAR.
 
+    //no hace falta ponerle tamanio al buffer ni a ingreso, se lo pone solo getline
+    //por lo menos en el tp1 no les puse y no tuve problema
+
+    usuario_t* usuario_activo = NULL;
+
+
     while(!terminar){
-        int nro_car = getline(&ingreso, &tam_buffer, stdin);
+        int longitud = getline(&ingreso, &tam_buffer, stdin);
+
         if (strcmp(ingreso, "login\n") == 0){
             printf("QUERÉS LOGEARTE CHIGADO?\n");
-            login();
+            login(usuarios,usuario_activo);
 
         }else if(strcmp(ingreso, "logout\n") == 0){
             printf("QUERÉS salir CHIGADO?\n");
+            logout(usuario_activo);
             debugger();
 
         }else if(strcmp(ingreso, "publicar\n") == 0){
@@ -96,7 +106,8 @@ void esperar_orden(){
         }else if(strcmp(ingreso, "mostrar_likes\n") == 0){
             printf("QUERÉS mostrar_likes CHIGADO?\n");
 
-        }else if(strcmp(ingreso, "quit\n") == 0){ // TAL VEZ QUITEAR SEA ingreso == NULL. -- VER
+        }else if(strcmp(ingreso, "quit\n") == 0){
+        // TAL VEZ QUITEAR SEA ingreso == NULL. -- VER
             printf("quitEAMOS CHIGADO!\n");
             terminar = true;
 
@@ -139,17 +150,33 @@ hash_t* guardar_usuarios_txt_hash(FILE* archivo){ // TERMINAR
 
 int main(int argc, char *argv[]){
     if (argc != NRO_ARGUMENTOS_INGRESO_TXT) {
-        printf("ERROR: el número de argumentos ingresados es erroneo."); 
+        printf("ERROR: el número de argumentos ingresados es erroneo.\n"); 
         return -1;
     }
     if (access(argv[ARGUMENTO_NOMBRE_ARCHIVO], R_OK) == -1) {//esto esta en py?? -- Nono, C de Cristiano. Cómo checkeaste vos si un archivo era valido en el tp1?
+
+    /* asi:
+
+    FILE* archi = fopen(direccion,"r");
+    if (!archi){
+        fprintf(stderr,"%s\n","Error: archivo fuente inaccesible");
+        return false;
+    }
+    */
+
         printf("Error: archivo fuente inaccesible"); 
         return -1;
     }
+
+
+
+
     FILE* archivo = fopen(argv[ARGUMENTO_NOMBRE_ARCHIVO], "r");
     hash_t* hash_usuarios = guardar_usuarios_txt_hash(archivo);
     fclose(archivo);
     
+
+
     esperar_orden();
 
     //hash_destruir(hash_usuarios);
